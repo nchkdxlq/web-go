d28cacbb34ed16292007d917598fb16fce7da741
Knox <nchkdxlq@gmail.com>
2020-07-15 09:03:27 +0800

    声明周期函数

--- react/learn/src/App.js
+++ react/learn/src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
 
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+    
+    this.state = {
+      count: 0
+    };
+  }
+
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentDidUpdate(preProps, preState, snapshot) {
+    console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  render() {
+    console.log('render');
+    const { count } = this.state;
+
+    return (
+      <div>
+        {"我是App组件"}
+        <h2>当前技术: {count}</h2>
+        <button onClick={ e => {this.increment()} }>+1</button>
+      </div>
+    )
+  }
+
+  increment() {
+    const { count } = this.state;
+    this.setState({
+      count: count + 1
+    });
+  }
 }
 
-export default App;
