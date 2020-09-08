5904eb96e4f2f5deffc09e124e880c6cc14feb49
Knox <nchkdxlq@gmail.com>
2020-09-08 08:55:27 +0800

    使用class组件重写TopBanner组件

--- react-netease-music/src/pages/discover/c-pages/recommend/c-cpns/top-banner/index.js
+++ react-netease-music/src/pages/discover/c-pages/recommend/c-cpns/top-banner/index.js
@@ -1,5 +1,5 @@
 import React, { memo, useState, useCallback, useEffect, useRef } from 'react';
-import { useSelector, useDispatch, shallowEqual } from 'react-redux';
+import { useSelector, useDispatch, shallowEqual, connect } from 'react-redux';
 
 import { getTopBannerAction } from '../../store/actionCreators';
 
@@ -11,50 +11,138 @@ import {
   BannerControl
 } from './style';
 
-export default memo(function TopBanner() {
+// export default memo(function TopBanner() {
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+//   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const { topBanners } = useSelector(state => {
-    return {topBanners: state.getIn(['recommend', 'topBanners'])}
-  }, shallowEqual)
+//   const { topBanners } = useSelector(state => {
+//     return {topBanners: state.getIn(['recommend', 'topBanners'])}
+//   }, shallowEqual)
 
-  const dispatch = useDispatch();
+//   const dispatch = useDispatch();
 
-  const bannerRef = useRef();
-  useEffect(() => {
-    dispatch(getTopBannerAction());
-  }, [dispatch]);
+//   const bannerRef = useRef();
+//   useEffect(() => {
+//     dispatch(getTopBannerAction());
+//   }, [dispatch]);
 
-  const bannerChange = useCallback((from, to) => {
-    setImmediate(() => setCurrentIndex(to));
-  }, [])
+//   const bannerChange = useCallback((from, to) => {
+//     setImmediate(() => setCurrentIndex(to));
+//   }, [])
 
-  const bgImage = topBanners[currentIndex] && (topBanners[currentIndex].imageUrl + "?imageView&blur=40x20")
+//   const bgImage = topBanners[currentIndex] && (topBanners[currentIndex].imageUrl + "?imageView&blur=40x20")
 
-  return (
-    <BannerWrapper bgImage={bgImage}>
-      <div className='banner wrap-v2'>
-        <BannerLeft>
-          <Carousel effect='fade' autoplay ref={bannerRef} beforeChange={bannerChange}>
-          {
-            topBanners.map((item, index) => {
-              return (
-                <div className="banner-item" key={item.imageUrl}>
-                  <img className="image" src={item.imageUrl} alt={item.typeTitle} />
-                </div>
-              )
-            })
-          }
-          </Carousel>
-        </BannerLeft>
-        <BannerRight />
-        <BannerControl>
-          <button className="btn left" onClick={e => bannerRef.current.prev()}></button>
-          <button className="btn right" onClick={e => bannerRef.current.next()}></button>
-        </BannerControl>
-      </div>
-    </BannerWrapper>
-  )
+//   return (
+//     <BannerWrapper bgImage={bgImage}>
+//       <div className='banner wrap-v2'>
+//         <BannerLeft>
+//           <Carousel effect='fade' autoplay ref={bannerRef} beforeChange={bannerChange}>
+//           {
+//             topBanners.map((item, index) => {
+//               return (
+//                 <div className="banner-item" key={item.imageUrl}>
+//                   <img className="image" src={item.imageUrl} alt={item.typeTitle} />
+//                 </div>
+//               )
+//             })
+//           }
+//           </Carousel>
+//         </BannerLeft>
+//         <BannerRight />
+//         <BannerControl>
+//           <button className="btn left" onClick={e => bannerRef.current.prev()}></button>
+//           <button className="btn right" onClick={e => bannerRef.current.next()}></button>
+//         </BannerControl>
+//       </div>
+//     </BannerWrapper>
+//   )
+// })
+
+
+
+
+class TopBanner extends React.PureComponent {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentIndex: 0,
+    };
+    this.carouselRef = null;
+  }
+
+  componentDidMount() {
+    const { fetchTopBanner } = this.props;
+    fetchTopBanner();
+  }
+
+  bannerChange = (from, to) => {
+    setImmediate(() => {
+      this.setState({
+        currentIndex: to
+      });
+    });
+  }
+
+  nextPage = () => {
+    this.carouselRef.next();
+  }
+
+  prePage = () => {
+    this.carouselRef.next();
+  }
+
+  render() {
+    const { topBanners } = this.props;
+    const { currentIndex } = this.state;
+    let bgImage = null;
+    if (topBanners && topBanners.length > currentIndex) {
+      bgImage = topBanners[currentIndex].imageUrl + "?imageView&blur=40x20";
+    }
+
+    return (
+      <BannerWrapper bgImage={bgImage}>
+        <div className='banner wrap-v2'>
+          <BannerLeft>
+            <Carousel effect='fade' autoplay ref={e => this.carouselRef = e} beforeChange={this.bannerChange}>
+            {
+              topBanners.map((item, index) => {
+                return (
+                  <div className="banner-item" key={item.imageUrl}>
+                    <img className="image" src={item.imageUrl} alt={item.typeTitle} />
+                  </div>
+                )
+              })
+            }
+            </Carousel>
+          </BannerLeft>
+          <BannerRight />
+          <BannerControl>
+            <button className="btn left" onClick={e => this.prePage()}></button>
+            <button className="btn right" onClick={e => this.nextPage()}></button>
+          </BannerControl>
+        </div>
+      </BannerWrapper>
+    )
+  }
+}
+
+// const mapStateToProps = state => {
+//   return {
+//     topBanners: state.getIn(['recommend', 'topBanners'])
+//   }
+// }
+
+// 返回当前组件需要使用的数据
+const mapStateToProps = state => ({
+  topBanners: state.getIn(['recommend', 'topBanners'])
 })
 
+
+// 返回当前组件需要使用的action
+const mapDispatchToProps = dispatch => ({
+  fetchTopBanner: () => { dispatch(getTopBannerAction()) }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopBanner);
