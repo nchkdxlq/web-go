578caaef04217297481da1e1bdd53d37f0345eca
Knox <nchkdxlq@gmail.com>
2020-11-02 08:51:56 +0800

    event loop

---
+++ node/learn/EventLoop/index.js
@@ -0,0 +1,74 @@
+
+function interview1() {
+
+  async function async1() {
+    console.log('async1 start');
+    await async2();
+    console.log('async1 end');
+  }
+
+  async function async2() {
+    console.log('async2');
+  }
+
+  console.log('script start');
+
+  setTimeout(function() {
+    console.log('setTimeout0');
+  }, 0);
+
+  setTimeout(function() {
+    console.log('setTimeout2');
+  }, 300);
+
+
+  setImmediate(() => console.log('setImmediate'));
+
+  process.nextTick(() => console.log('nextTick1'));
+
+  async1();
+
+  process.nextTick(() => console.log('nextTick2'));
+
+  new Promise(function (resolve){
+    console.log('promise1');
+    resolve();
+    console.log('promise2');
+  }).then(function () {
+    console.log('promise3');
+  })
+
+  console.log('script end');
+}
+
+// interview1();
+/* 运行结果
+script start
+async1 start
+async2
+promise1
+promise2
+script end
+nextTick1
+nextTick2
+async1 end
+promise3
+setTimeout0
+setImmediate
+setTimeout2
+*/
+
+
+
+function interview2() {
+
+  setTimeout(() => {
+    console.log('setTimeout');
+  }, 0);
+
+  setImmediate(() => {
+    console.log('setImmediate');
+  });
+}
+
+interview2();
