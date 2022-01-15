0f44ae1e025d1447f7327b8caddc100eac2e161e
Knox <nchkdxlq@gmail.com>
2022-01-15 12:36:30 +0800

    typescript hello world

---
+++ TypeScript/build/webpack.dev.config.js
@@ -0,0 +1,11 @@
+const webpack = require('webpack');
+
+module.exports = {
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                devtools: 'cheap-module-eval-source-map'
+            }
+        })
+    ]
+}
